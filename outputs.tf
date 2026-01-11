34223d42e90ff5dd0c2597c850b918683e91419c
Daniel Allen <dan@aeternagroup.net>
2026-01-11 01:34:03 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "capacity_reservation_groups" {
+  description = "All capacity_reservation_group resources"
+  value       = azurerm_capacity_reservation_group.capacity_reservation_groups
+}
+
