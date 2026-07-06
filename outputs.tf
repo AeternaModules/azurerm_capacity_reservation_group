3aac5b05f6270cf1e8513eb20e970e49df226c39
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:11:32 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,24 @@ output "capacity_reservation_groups" {
   description = "All capacity_reservation_group resources"
   value       = azurerm_capacity_reservation_group.capacity_reservation_groups
 }
+output "capacity_reservation_groups_location" {
+  description = "List of location values across all capacity_reservation_groups"
+  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.location]
+}
+output "capacity_reservation_groups_name" {
+  description = "List of name values across all capacity_reservation_groups"
+  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.name]
+}
+output "capacity_reservation_groups_resource_group_name" {
+  description = "List of resource_group_name values across all capacity_reservation_groups"
+  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.resource_group_name]
+}
+output "capacity_reservation_groups_tags" {
+  description = "List of tags values across all capacity_reservation_groups"
+  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.tags]
+}
+output "capacity_reservation_groups_zones" {
+  description = "List of zones values across all capacity_reservation_groups"
+  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.zones]
+}
 
