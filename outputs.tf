e57f013fda191c97a9757bdda7c8faa3c18726a6
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:16:17 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "capacity_reservation_groups" {
-  description = "All capacity_reservation_group resources"
-  value       = azurerm_capacity_reservation_group.capacity_reservation_groups
-}
 output "capacity_reservation_groups_location" {
-  description = "List of location values across all capacity_reservation_groups"
-  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.location]
+  description = "Map of location values across all capacity_reservation_groups, keyed the same as var.capacity_reservation_groups"
+  value       = { for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : k => v.location }
 }
 output "capacity_reservation_groups_name" {
-  description = "List of name values across all capacity_reservation_groups"
-  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.name]
+  description = "Map of name values across all capacity_reservation_groups, keyed the same as var.capacity_reservation_groups"
+  value       = { for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : k => v.name }
 }
 output "capacity_reservation_groups_resource_group_name" {
-  description = "List of resource_group_name values across all capacity_reservation_groups"
-  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.resource_group_name]
+  description = "Map of resource_group_name values across all capacity_reservation_groups, keyed the same as var.capacity_reservation_groups"
+  value       = { for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : k => v.resource_group_name }
 }
 output "capacity_reservation_groups_tags" {
-  description = "List of tags values across all capacity_reservation_groups"
-  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.tags]
+  description = "Map of tags values across all capacity_reservation_groups, keyed the same as var.capacity_reservation_groups"
+  value       = { for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : k => v.tags }
 }
 output "capacity_reservation_groups_zones" {
-  description = "List of zones values across all capacity_reservation_groups"
-  value       = [for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : v.zones]
+  description = "Map of zones values across all capacity_reservation_groups, keyed the same as var.capacity_reservation_groups"
+  value       = { for k, v in azurerm_capacity_reservation_group.capacity_reservation_groups : k => v.zones }
 }
 
